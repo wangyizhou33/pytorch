b71c8a519ecc1ddf0e2e81aa1efdd93a3ea31434
Yizhou Wang <wangyizhou3@gmail.com>
2025-01-13 14:39:21 -0800

    huggingface

---
+++ huggingface/sentiment.py
@@ -0,0 +1,7 @@
+from transformers import pipeline
+
+classifier = pipeline("sentiment-analysis")
+output = classifier(
+    ["I've been waiting for a HuggingFace course my whole life.", "I hate this so much!"]
+)
+print(output)
---
+++ huggingface/zero-shot.py
@@ -0,0 +1,8 @@
+from transformers import pipeline
+
+classifier = pipeline("zero-shot-classification")
+output = classifier(
+    "This is a course about the Transformers library",
+    candidate_labels=["education", "politics", "business"],
+)
+print(output)
